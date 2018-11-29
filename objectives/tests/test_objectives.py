6f8015edae05c033244a1e3a20793b256c1bd8fb
MariaCristina16 <criucc@tiscali.it>
2018-11-29 20:43:24 +0100

    Update test_objectives.py
    Change to previous state

--- objectives/tests/test_objectives.py
+++ objectives/tests/test_objectives.py
@@ -6,7 +6,7 @@ from objectives.database import db, Objective
 
 
 DATASERVICE=os.environ['DATA_SERVICE']
-OBJECTIVESERVICE=os.environ['OBJECTIVE_SERVICE']
+
 
 def test_objective(client):
     tested_app, app = client
@@ -115,7 +115,7 @@ def test_delete_objective_by_user(client):
         objective = db.session.query(Objective).first()
 
     with requests_mock.mock() as m:
-        m.get(OBJECTIVESERVICE + '/objectives', json=[])
+        m.get(DATASERVICE + '/objectives', json=[])
 
         assert tested_app.delete('/objectives?user_id=1').status_code == 200
 
